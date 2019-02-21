0c362007a75fbb27e4fbe4263ce7f5b127999641
landauleo <bambie7@mail.ru>
2019-02-21 11:05:24 +0300

    у меня 9 ошибок и 20 ворнингов, кажется, я так ничего и не поняла

--- src/main/java/Family.java
+++ src/main/java/Family.java
@@ -1,57 +1,43 @@
 //Attempt to create a nuclear "family" in order to practice classes and objects.
 
-public class Family {
+
 
     public static void main (String[] args) {
 
-        FamilyMember mom = new FamilyMember();
-        mom.age = 46;
-        mom.eyeColour = "blue";
-        mom.hairColour = "blond";
-        mom.name = "Jane";
-        mom.nickname = "Dear";
-        mom.height = 167.5;
 
-        FamilyMember dad = new FamilyMember();
-        dad.name = "Dick";
-        dad.nickname = "Almighty";
-        dad.hairColour = "dark blond";
-        dad.eyeColour = "green";
-        dad.age = 50;
-        dad.height = 186.3;
+     class Mom {
+        String eyeColour;
+        String hairColour;
 
-        FamilyMember eldestDaughter = new FamilyMember();
-        eldestDaughter.name = "Angie";
-        eldestDaughter.height = 165.9;
-        eldestDaughter.nickname = "Sweetie";
-        eldestDaughter.age = 23;
-        eldestDaughter.eyeColour = "blue";
-
-
-        FamilyMember middleDaughter = new FamilyMember();
-        middleDaughter.eyeColour = "green";
-        middleDaughter.age = 17;
-        middleDaughter.name = "Johanna";
-        middleDaughter.nickname = "Hey, you";
-
-        FamilyMember youngestDaughter = new FamilyMember();
-        youngestDaughter.nickname = "Baby";
-        youngestDaughter.name = "Dora";
-        youngestDaughter.age = 4;
-
-        FamilyMember son = new FamilyMember();
-        son.name = "Prokopiy";
-        son.nickname = "Asshole";
+        Mom(String a, String b) {
+            a = "blue";
+            b = "blond";
+        }
     }
 
-    public static class FamilyMember {
+      class Dad {
+        String eyeColour;
+        String hairColour;
 
-        public String name;
-        public String nickname;
-        public int age;
-        public String eyeColour;
-        public String hairColour;
-        public double height;
+        Dad(String c, String d) {
+            c = "brown";
+            d = "black";
+        }
+
     }
+
+
+    class Daughter extends Mom {
+        String a, b;
+        @Override
+        String a = "grey";
+    }
+
+        class Son extends Dad {
+        String c, d;
+        @Override
+        String d = "red";
+    }
+
 }
 
